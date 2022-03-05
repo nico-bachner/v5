fb5b7424c9320ce95ae286d44ff9be88d855d25b
Nico Bachner <mail@nbac.me>
2022-03-05 17:42:06 +0100

    feat: add hover animation to Nav items

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -6,7 +6,7 @@ type NavItemProps = {
 
 export const NavItem: React.FC<NavItemProps> = ({ children, href }) => (
   <Link href={href}>
-    <a className="box-content w-8 rounded-lg border border-white/20 bg-white/25 p-3 shadow-lg backdrop-blur-sm">
+    <a className="box-content w-8 transform rounded-lg border border-white/20 bg-white/25 p-3 shadow-lg backdrop-blur-sm transition hover:scale-105">
       {children}
     </a>
   </Link>
