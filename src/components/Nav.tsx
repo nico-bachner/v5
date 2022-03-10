3b532caf203363236f4114d9ff4afab53680df9d
Nico Bachner <mail@nbac.me>
2022-03-10 13:59:04 +0100

    fix: remove timeline

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,16 @@
-import {
-  HomeIcon,
-  CollectionIcon,
-  PencilIcon,
-  CalendarIcon,
-} from '@heroicons/react/outline'
+import { HomeIcon, CollectionIcon, PencilIcon } from '@heroicons/react/outline'
 import { NavItem } from './NavItem'
 
 export const Nav: React.VFC = () => (
   <nav className="fixed bottom-8 left-0 right-0 z-50 mx-auto flex w-fit gap-3 rounded-xl border border-white/20 bg-white/25 p-2 shadow-lg backdrop-blur-sm">
-    <NavItem href="/">
-      <HomeIcon />
-    </NavItem>
     <NavItem href="/projects">
       <CollectionIcon />
     </NavItem>
+    <NavItem href="/">
+      <HomeIcon />
+    </NavItem>
     <NavItem href="/writing">
       <PencilIcon />
-    </NavItem>
-    <NavItem href="/timeline">
-      <CalendarIcon />
     </NavItem>
   </nav>
 )
