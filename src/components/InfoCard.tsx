d70d4606ec42bb1cef0d8618aecf825dc7741de9
Nico Bachner <mail@nbac.me>
2022-03-12 16:26:51 +0100

    fix(design): adjust Card spacing

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -16,8 +16,8 @@ export const InfoCard: React.VFC<InfoCardProps> = ({
   info,
 }) => (
   <Link href={href}>
-    <a className="flex transform flex-col gap-4 rounded-lg border border-white/20 bg-white/25 p-6 shadow-lg backdrop-blur-sm md:p-10 md:transition md:duration-300 md:hover:scale-105 md:hover:shadow-xl lg:p-12">
-      <p className="text-2xl font-extrabold tracking-tight md:text-3xl lg:text-4xl">
+    <a className="flex transform flex-col gap-2 rounded-lg border border-white/20 bg-white/25 p-6 shadow-lg backdrop-blur-sm md:gap-4 md:p-10 md:transition md:duration-300 md:hover:scale-105 md:hover:shadow-xl lg:gap-6 lg:p-12">
+      <p className="text-2xl font-extrabold md:text-3xl md:tracking-tight lg:text-4xl">
         <strong>{header}</strong>
       </p>
       <p className="prose prose-slate md:prose-lg lg:prose-xl">{body}</p>
