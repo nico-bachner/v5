9c71fa4411c976f89b7cb6ea4a08e5060a3282c3
Nico Bachner <mail@nbac.me>
2022-03-14 22:24:12 +0100

    fix(a11y): add link titles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -15,28 +15,38 @@ export const Nav: React.VFC = () => {
   return (
     <nav className="fixed bottom-4 left-0 right-0 z-20 mx-auto flex w-fit gap-3 rounded-xl border border-white/20 bg-white/30 p-2 shadow-lg backdrop-blur-sm dark:border-zinc-600/20 dark:bg-zinc-600/30 md:bottom-8">
       <Link href="/">
-        <a className="transform rounded-lg border border-white/20 bg-white/30 p-3 shadow-lg backdrop-blur-sm transition hover:scale-105 dark:border-zinc-600/20 dark:bg-zinc-600/30">
+        <a
+          title="Home"
+          className="transform rounded-lg border border-white/20 bg-white/30 p-3 shadow-lg backdrop-blur-sm transition hover:scale-105 dark:border-zinc-600/20 dark:bg-zinc-600/30"
+        >
           <HomeIcon className="h-8 w-8" />
         </a>
       </Link>
       <Link href="/projects">
-        <a className="transform rounded-lg border border-white/20 bg-white/30 p-3 shadow-lg backdrop-blur-sm transition hover:scale-105 dark:border-zinc-600/20 dark:bg-zinc-600/30">
+        <a
+          title="Projects"
+          className="transform rounded-lg border border-white/20 bg-white/30 p-3 shadow-lg backdrop-blur-sm transition hover:scale-105 dark:border-zinc-600/20 dark:bg-zinc-600/30"
+        >
           <CollectionIcon className="h-8 w-8" />
         </a>
       </Link>
       <Link href="/writing">
-        <a className="transform rounded-lg border border-white/20 bg-white/30 p-3 shadow-lg backdrop-blur-sm transition hover:scale-105 dark:border-zinc-600/20 dark:bg-zinc-600/30">
+        <a
+          title="Writing"
+          className="transform rounded-lg border border-white/20 bg-white/30 p-3 shadow-lg backdrop-blur-sm transition hover:scale-105 dark:border-zinc-600/20 dark:bg-zinc-600/30"
+        >
           <PencilIcon className="h-8 w-8" />
         </a>
       </Link>
-      <div
+      <button
+        title="Command Menu"
         onClick={() => {
           setIsOpen(!isOpen)
         }}
         className="transform cursor-pointer rounded-lg border border-white/20 bg-white/30 p-3 shadow-lg backdrop-blur-sm transition hover:scale-105 dark:border-zinc-600/20 dark:bg-zinc-600/30"
       >
         <AdjustmentsIcon className="h-8 w-8" />
-      </div>
+      </button>
     </nav>
   )
 }
