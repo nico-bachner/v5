a9f8839958fc30745e59806588fc8aa828e3e3bd
Nico Bachner <git@nbac.me>
2022-04-25 01:39:12 +0200

    feat: add Next.js Link to in-content links

--- src/components/MDX.tsx
+++ src/components/MDX.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { MDXRemote } from 'next-mdx-remote'
 
 import type { MDXContent } from 'lib/mdx'
@@ -13,11 +14,23 @@ export const MDX: React.VFC<MDXProps> = ({ content, components }) => (
     {...content}
     components={{
       wrapper: ({ children }) => (
-        <div className="prose prose-zinc prose-headings:scroll-mt-[1.5em] prose-a:text-blue-500 dark:prose-invert sm:prose-lg lg:prose-xl">
+        <div className="prose prose-zinc prose-headings:scroll-mt-6 prose-a:text-blue-500 dark:prose-invert sm:prose-lg lg:prose-xl">
           {children}
         </div>
       ),
       Image,
+      a: ({ children, href }) =>
+        href?.startsWith('/') ? (
+          <Link href={href}>
+            <a>{children}</a>
+          </Link>
+        ) : href?.startsWith('#') ? (
+          <a href={href}>{children}</a>
+        ) : (
+          <a href={href} target="_blank" rel="noopener noreferrer">
+            {children}
+          </a>
+        ),
 
       ...components,
     }}
