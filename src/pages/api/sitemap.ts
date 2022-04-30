a122a46d8bea7e3928259cd94e90fad63d66b80b
Nico Bachner <git@nbac.me>
2022-04-30 15:32:19 +0200

    refactor: restructure sitemap api route

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -2,47 +2,44 @@ import { fetchPagesData } from 'lib/data/pages'
 
 import type { NextApiHandler } from 'next'
 
-export const apiHandler: NextApiHandler = async (req, res) => {
+const apiHandler: NextApiHandler = async (req, res) => {
   const pages = await fetchPagesData()
 
-  res.status(200)
-  res.setHeader('Content-Type', 'text/xml')
-  res.end(`<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-      <loc>https://nicobachner.com</loc>
-      <changefreq>monthly</changefreq>
-      <priority>1.0</priority>
-      </url>
-      <url>
-      <loc>https://nicobachner.com/projects</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-      </url>
-      <url>
-      <loc>https://nicobachner.com/pages</loc>
-      <changefreq>weekly</changefreq>
-      <priority>0.8</priority>
-      </url>
-      ${pages
-        .map(
-          ({ path, updated }) => `
-          <url>
-              <loc>https://nicobachner.com/${path.join('/')}</loc>
-              ${
-                updated
-                  ? `<lastmod>${new Date(updated)
-                      .toISOString()
-                      .slice(0, 10)}</lastmod>`
-                  : ''
-              }
-              <priority>0.5</priority>
-          </url>
-          `
-        )
-        .join('')}
-    </urlset>
-  `)
+  res.status(200).setHeader('Content-Type', 'text/xml')
+    .end(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>https://nicobachner.com</loc>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>https://nicobachner.com/projects</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://nicobachner.com/pages</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  ${pages
+    .map(
+      ({ path, updated }) => `
+  <url>
+    <loc>https://nicobachner.com/${path.join('/')}</loc>
+    <lastmod>${
+      updated ? new Date(updated).toISOString().slice(0, 10) : 'N/A'
+    }</lastmod>
+  </url>
+  `
+    )
+    .join('')}
+</urlset>
+`)
 }
 
 export default apiHandler
